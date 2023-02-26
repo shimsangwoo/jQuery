55646ff3eea93d8dcc24b69ec86cf1b19132557c
sangwoo <sw_shim@hanmail.net>
2023-02-26 11:23:30 +0900

    20230226

---
+++ 00_basic/00_basic/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    var btn = $('.btn')
+    btn.on('click', function(){
+        alert('hello')
+    })    
+  
+  });
+
+
+
+  /* 최근 변경된 형식*/
+$(function){
+
+}
+
+
+
+  /* 항상 달라 도큐먼트로 감싸줘야한다(도큐먼트_레디_펑션). 헤드에 집어넣으면 읽고 나가버린다. */
+  /* 이제 공식서류에서 없어졌다.*/
+
+
+
+
